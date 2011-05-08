0e7a61b008eb83d39e5df50c917c230626d12153
Bob McClellan Jr <bob.mcclellan@gmail.com>
2011-05-08 01:47:07 -0500

    Initial commit of site.

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+# Bundle edge Rails instead:
+gem 'rails', :git => 'git://github.com/rails/rails.git'
+gem 'pg'
+
+# Deploy with Capistrano
+gem 'capistrano'
+
+group :development, :test do
+  gem 'webrat'
+  gem 'ruby-debug19'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FifteenthOrg::Application.config.secret_token = 'ac85eb74312db63911bd7bd2425522dea56126387cd57258ec6a460edde4d4f4e06759eea4d78b566c7bd1575b5438893fee13e3b32c7d13dcce881bf735ba3a'
